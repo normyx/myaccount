fd5b7ad476de801285fd8a73a8dae6158aca667c
Unknown <mathieu.goulene@gmail.com>
2018-08-08 09:17:43 +0200

    minor enhancements

--- src/main/webapp/app/dashboard-ui-component/amount-evolution-in-month.component.ts
+++ src/main/webapp/app/dashboard-ui-component/amount-evolution-in-month.component.ts
@@ -7,83 +7,76 @@ import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
 import * as moment from 'moment';
 
 @Component({
-  selector: 'jhi-amount-evolution-in-month',
-  templateUrl: './amount-evolution-in-month.component.html'
+    selector: 'jhi-amount-evolution-in-month',
+    templateUrl: './amount-evolution-in-month.component.html'
 })
 export class AmountEvolutionInMonthComponent implements OnInit, OnChanges {
+    @Input() accountId: number;
+    @Input() month: string;
+    // accountCategoryMonthReport: IAccountCategoryMonthReport;
+    data: any;
+    options: any;
 
-  @Input() accountId: number;
-  @Input() month: moment.Moment;
-  // accountCategoryMonthReport: IAccountCategoryMonthReport;
-  data: any;
-  options: any;
+    constructor(private dashboardUIComponentsService: DashboardUIComponentsService, private jhiAlertService: JhiAlertService) {}
 
-  constructor(
-    private dashboardUIComponentsService: DashboardUIComponentsService,
-    private jhiAlertService: JhiAlertService,
-  ) { }
-
-  loadAll() {
-    if (this.accountId && this.month) {
-      this.dashboardUIComponentsService
-        .getDataDatesWhereMonth(this.accountId, moment('2018-07-01'))
-        .subscribe(
-          (res: HttpResponse<any>) => {
-            this.data = {
-              labels: res.body.dates,
-              datasets: [
-                {
-                  label: 'Operation',
-                  data: res.body.operationAmounts,
-                  borderColor: '#0099ff',
-                  backgroundColor: '#0099ff',
-                  fill: false
+    loadAll() {
+        if (this.accountId && this.month) {
+            this.dashboardUIComponentsService.getDataDatesWhereMonth(this.accountId, moment(this.month)).subscribe(
+                (res: HttpResponse<any>) => {
+                    this.data = {
+                        labels: res.body.dates,
+                        datasets: [
+                            {
+                                label: 'Operation',
+                                data: res.body.operationAmounts,
+                                borderColor: '#0099ff',
+                                backgroundColor: '#0099ff',
+                                fill: false
+                            },
+                            {
+                                label: 'Budget',
+                                data: res.body.budgetAmounts,
+                                borderColor: '#565656',
+                                backgroundColor: '#565656',
+                                borderWidth: 1,
+                                fill: false
+                            },
+                            {
+                                label: 'Evolution prévue',
+                                data: res.body.predictiveBudgetAmounts,
+                                borderColor: '#ff0000',
+                                backgroundColor: '#ff0000',
+                                fill: false
+                            }
+                        ]
+                    };
+                    this.options = {
+                        title: {
+                            display: true,
+                            text: res.body.month,
+                            fontSize: 16
+                        },
+                        legend: {
+                            position: 'bottom'
+                        },
+                        tooltips: {
+                            position: 'average',
+                            mode: 'index',
+                            intersect: false
+                        }
+                    };
                 },
-                {
-                  label: 'Budget',
-                  data: res.body.budgetAmounts,
-                  borderColor: '#565656',
-                  backgroundColor: '#565656',
-                  borderWidth: 1,
-                  fill: false
-                },
-                {
-                  label: 'Evolution prévue',
-                  data: res.body.predictiveBudgetAmounts,
-                  borderColor: '#ff0000',
-                  backgroundColor: '#ff0000',
-                  fill: false,
-                }
-              ]
-            };
-            this.options = {
-              title: {
-                display: true,
-                text: res.body.month,
-                fontSize: 16
-              },
-              legend: {
-                position: 'bottom'
-              },
-              tooltips: {
-                position: 'average',
-                mode: 'index',
-                intersect: false,
-              },
-            };
-          },
-          (res: HttpErrorResponse) => this.onError(res.message)
-        );
+                (res: HttpErrorResponse) => this.onError(res.message)
+            );
+        }
     }
-  }
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
-  ngOnChanges() {
-    this.loadAll();
-  }
-  private onError(errorMessage: string) {
-    this.jhiAlertService.error(errorMessage, null, null);
-  }
+    ngOnChanges() {
+        this.loadAll();
+    }
+    private onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
+    }
 }
