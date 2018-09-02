9342a68cbc13bc7421d97d14a72349ad41876cc7
vagrant <vagrant@vagrant.vm>
2018-09-02 23:52:38 +0200

    move dev db from mysql to h2

--- src/main/java/org/mgoulene/config/WebConfigurer.java
+++ src/main/java/org/mgoulene/config/WebConfigurer.java
@@ -2,6 +2,7 @@ package org.mgoulene.config;
 
 import io.github.jhipster.config.JHipsterConstants;
 import io.github.jhipster.config.JHipsterProperties;
+import io.github.jhipster.config.h2.H2ConfigurationHelper;
 import io.github.jhipster.web.filter.CachingHttpHeadersFilter;
 
 import com.codahale.metrics.MetricRegistry;
@@ -61,6 +62,9 @@ public class WebConfigurer implements ServletContextInitializer, WebServerFactor
         initMetrics(servletContext, disps);
         if (env.acceptsProfiles(JHipsterConstants.SPRING_PROFILE_PRODUCTION)) {
             initCachingHttpHeadersFilter(servletContext, disps);
+        }
+        if (env.acceptsProfiles(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)) {
+            initH2Console(servletContext);
         }
         log.info("Web application fully configured");
     }
@@ -186,6 +190,14 @@ public class WebConfigurer implements ServletContextInitializer, WebServerFactor
             source.registerCorsConfiguration("/v2/api-docs", config);
         }
         return new CorsFilter(source);
+    }
+
+        /**
+     * Initializes H2 console.
+     */
+    private void initH2Console(ServletContext servletContext) {
+        log.debug("Initialize H2 console");
+        H2ConfigurationHelper.initH2Console(servletContext);
     }
 
     @Autowired(required = false)
