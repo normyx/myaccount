de36fc2dde3cef88b5946e193420fc5edcfb23f4
Unknown <mathieu.goulene@gmail.com>
2018-11-07 08:31:03 +0100

    Add Angular Material Design

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -3,13 +3,14 @@ import { NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
 
 import { NgbDateMomentAdapter } from './util/datepicker-adapter';
 import { MyaccountSharedLibsModule, MyaccountSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective } from './';
+import { JhiMaterialModule } from 'app/shared/jhi-material.module';
 
 @NgModule({
-    imports: [MyaccountSharedLibsModule, MyaccountSharedCommonModule],
+    imports: [MyaccountSharedLibsModule, MyaccountSharedCommonModule, JhiMaterialModule],
     declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective],
     providers: [{ provide: NgbDateAdapter, useClass: NgbDateMomentAdapter }],
     entryComponents: [JhiLoginModalComponent],
-    exports: [MyaccountSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective],
+    exports: [MyaccountSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective, JhiMaterialModule],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class MyaccountSharedModule {}
